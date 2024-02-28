7a3254393d71362a0a1b0f6ca37271b1c9cafbbd
yukselnevfel <nyuksel3410@gmail.com>
2024-02-28 17:22:38 +0300

    ilk kurulum şekli

---
+++ appiumKontrol-main/src/test/java/Calculator_Test.java
@@ -0,0 +1,34 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class Calculator_Test {
+
+
+    AndroidDriver<AndroidElement> driver; //android cihazlarin driveri
+
+    //  AppiumDriver<MobileElement> appiumDriver; //hem android de hem de ios da calisir
+
+    @Test
+    public void deneme1() throws MalformedURLException {
+
+        DesiredCapabilities capabilities=new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"PIXEL 2");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
+        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\Nevfel\\IdeaProjects\\appiumKontrol-main\\appiumKontrol-main\\Apps\\Calculator_8.4 (503542421)_Apkpure.apk");
+
+        driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+
+    }
