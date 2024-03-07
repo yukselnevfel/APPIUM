98e78b1e9277cb2ff5deada4fbba794a755a697a
yukselnevfel <nyuksel3410@gmail.com>
2024-03-07 11:05:55 +0300

    düzeltme

--- appiumKontrol-main/src/test/java/tests/AllCurrencyConverterApp.java
+++ appiumKontrol-main/src/test/java/tests/AllCurrencyConverterApp.java
@@ -2,6 +2,7 @@ package tests;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import utilities.Driver;
 
@@ -10,8 +11,8 @@ public class AllCurrencyConverterApp {
 
     @Test
     public void allCurrencyTest(){
-
 // all currency uygulamasinin yuklendigi dogulanir
+        Assert.assertTrue( driver.isAppInstalled("com.smartwho.SmartAllCurrencyConverter"));
 // uygulamanin acildigi dogrulanir
 // cevirmek istedigimiz para birimi zloty olarak secilir
 // cevirelecek olan para birimi Tl olarak secilir
--- appiumKontrol-main/src/test/java/tests/KiwiApp.java
+++ appiumKontrol-main/src/test/java/tests/KiwiApp.java
@@ -7,7 +7,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.KiwiPage;
 import utilities.Driver;
-import utilities.ResuableMethods;
+import utilities.ReusableMethods;
 
 public class KiwiApp {
     AndroidDriver<AndroidElement>driver=Driver.getAndroidDriver();
@@ -30,15 +30,15 @@ public class KiwiApp {
         Thread.sleep(1000);
 // ardinda gelecek olan 3 adimada yesil butona basilarak devam edilir
         for (int i=0; i<3; i++){
-            ResuableMethods.koordinatTiklamaMethodu(541,1700,1000);
+            ReusableMethods.koordinatTiklamaMethodu(541,1700,1000);
         }
         Thread.sleep(2500);
 // Trip type,one way olarak secilir
-        ResuableMethods.koordinatTiklamaMethodu(300,617,1000);
-        ResuableMethods.koordinatTiklamaMethodu(508,1461,1000);
+        ReusableMethods.koordinatTiklamaMethodu(300,617,1000);
+        ReusableMethods.koordinatTiklamaMethodu(508,1461,1000);
 // kalkis ulkesi secenegine tiklanir ve default olan ulke kaldirilir
-        ResuableMethods.koordinatTiklamaMethodu(504,782,1000);
-        ResuableMethods.koordinatTiklamaMethodu(1019,139,1000);
+        ReusableMethods.koordinatTiklamaMethodu(504,782,1000);
+        ReusableMethods.koordinatTiklamaMethodu(1019,139,1000);
 // kalkis yapilacak ulke/sehir girilir ve sec e tiklanir
         if (!driver.isKeyboardShown()){
             page.kalkisTextKutusu.sendKeys("Ankara");
@@ -46,29 +46,29 @@ public class KiwiApp {
             driver.getKeyboard().pressKey("Istanbul");
         }
         Thread.sleep(1500);
-        ResuableMethods.koordinatTiklamaMethodu(482,289,1000);
+        ReusableMethods.koordinatTiklamaMethodu(482,289,1000);
         page.chooseButton.click();
         Thread.sleep(1000);
 
 // varis ulkesi secenegine tiklanir ve gidilecek ulke girilir
-        ResuableMethods.koordinatTiklamaMethodu(508,917,1000);
+        ReusableMethods.koordinatTiklamaMethodu(508,917,1000);
         driver.getKeyboard().pressKey("Antalya");
         Thread.sleep(1500);
-        ResuableMethods.koordinatTiklamaMethodu(482,289,1000);
+        ReusableMethods.koordinatTiklamaMethodu(482,289,1000);
         page.chooseButton.click();
         Thread.sleep(1000);
 // gidis tarihi eylul ayinin 21 i olarak secilir ve set date e tiklanir
-        ResuableMethods.koordinatTiklamaMethodu(537,1048,1000);
-        ResuableMethods.koordinatTiklamaMethodu(541,1140,1000);
-        ResuableMethods.koordinatTiklamaMethodu(716,1721,1000);
+        ReusableMethods.koordinatTiklamaMethodu(537,1048,1000);
+        ReusableMethods.koordinatTiklamaMethodu(541,1140,1000);
+        ReusableMethods.koordinatTiklamaMethodu(716,1721,1000);
 
 // search butonuna tiklanir
-        ResuableMethods.koordinatTiklamaMethodu(541,1205,3000);
+        ReusableMethods.koordinatTiklamaMethodu(541,1205,3000);
 // en  ucuz ve aktarmasiz filtrelemeleri yapilir
-        ResuableMethods.koordinatTiklamaMethodu(252,252,1000);
-        ResuableMethods.koordinatTiklamaMethodu(563,584,2000);
-        ResuableMethods.koordinatTiklamaMethodu(519,256,1000);
-        ResuableMethods.koordinatTiklamaMethodu(511,1458,3000);
+        ReusableMethods.koordinatTiklamaMethodu(252,252,1000);
+        ReusableMethods.koordinatTiklamaMethodu(563,584,2000);
+        ReusableMethods.koordinatTiklamaMethodu(519,256,1000);
+        ReusableMethods.koordinatTiklamaMethodu(511,1458,3000);
 
 // gelen bilet fiyati kaydedilir ve kullanicin telefonuna sms olarak gonderilir
         String biletFiyati=page.sonucFiyat.getText();
